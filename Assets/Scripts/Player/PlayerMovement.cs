40f0a7ffe57b3cb25e7f2732328c74d5be09cdb0
Zodiac411 <thechriszone@icloud.com>
2023-11-09 11:55:22 +0000

    FIxed movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,21 +7,51 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
-    Vector3 velocity;
+    public float mouseSensitivity = 100f;
 
-  
-    void FixedUpdate()
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    Vector3 velocity;
+    float xRotation = 0f;
+
+    void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+    }
+
+    void Update()
+    {
+        // Ground Check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Setting a small negative value to keep the player grounded
+        }
+
+        // Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
-        velocity.y += gravity * Time.deltaTime;
 
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 17f;
+        }
+        else
+        {
+            speed = 12f;
+        }
     }
 }
